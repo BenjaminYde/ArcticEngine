26b1583d2780a14d7a465bb1027c7c110f9a1b59
Benjamin Yde <benjamin.yde@gmail.com>
2022-10-30 01:19:29 +0200

    cmake: added package: glm

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <vulkan/vulkan.h>
 #include <format>
+#include "glm/vec3.hpp"
+#include "glm/geometric.hpp"
 
 int main()
 {
     VkExtent2D windowExtend{ 1280 , 720 };
+    glm::vec3 vector = glm::vec3(0.5f, 0.5f, 0.5f);
 
     std::cout << "Hello World!" << std::endl;
-    std::cout << std::format("Window extend w: {} h: {}", windowExtend.width, windowExtend.height) << std::endl;
+    std::cout << std::format("vk window extend w: {} h: {}", windowExtend.width, windowExtend.height) << std::endl;
+    std::cout << std::format("glm vector length: {}", glm::length(vector)) << std::endl;
     return 0;
 }
