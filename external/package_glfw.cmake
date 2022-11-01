28e59d777aa1d576b534a37ad513d268d15ddebc
Benjamin Yde <benjamin.yde@gmail.com>
2022-11-01 17:26:51 +0100

    cmake: refactoring: added folders (external + src)

---
+++ external/package_glfw.cmake
@@ -0,0 +1,14 @@
+# function to find vulkan
+function(FindPackage_GLFW TARGET_NAME)
+    FetchContent_Declare(external_glfw
+            GIT_REPOSITORY    https://github.com/glfw/glfw
+            GIT_TAG           3.3.8)
+
+    set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
+    set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+    set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+
+    FetchContent_MakeAvailable(external_glfw)
+
+    target_link_libraries(${TARGET_NAME} PRIVATE glfw)
+endfunction()
