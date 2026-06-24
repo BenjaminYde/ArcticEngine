a6eb59e4291b7dad80784c0921e12e0e54f1d5a2
Benjamin Yde <benjamin.yde@vintecc.com>
2026-06-24 19:51:20 +0000

    feat: working instance creation

--- vulkan_tutorial_2026/docs/resources.md
+++ vulkan_tutorial_2026/docs/resources.md
@@ -1,7 +1,19 @@
 ## CMake
 
-Vulkan:
-
 - [Vulkan Docs: CMake](https://docs.vulkan.org/tutorial/latest/02_Development_environment.html#_cmake)
 - [FindVulkan](https://cmake.org/cmake/help/latest/module/FindVulkan.html)
 - [Mathieu Ropert: C++ modules_in_2026](https://mropert.github.io/2026/04/13/modules_in_2026/)
+
+## SDL3
+
+- [mropert/vk-renderer](https://github.com/mropert/vk-renderer)
+- [stevelittlefish/c_vulkan_sdl3](https://github.com/stevelittlefish/c_vulkan_sdl3/blob/main/README.md)
+
+## Vulkan
+
+- [KhronosGroup: Vulkan-Hpp: Usage](https://github.com/KhronosGroup/Vulkan-Hpp/blob/main/docs/Usage.md)
+- [KhronosGroup: Vulkan-Hpp: VkRaiiProgrammingGuide](https://github.com/KhronosGroup/Vulkan-Hpp/blob/main/docs/VkRaiiProgrammingGuide.md)
+
+## Vulkan Validation Layers
+
+- [LunarG: Layer Configuration](https://vulkan.lunarg.com/doc/view/latest/windows/layer_configuration.html)
--- vulkan_tutorial_2026/src/main.cpp
+++ vulkan_tutorial_2026/src/main.cpp
@@ -1,20 +1,40 @@
+// C++ standard library
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <exception>
+#include <string_view>
+import std;
+
+// Vulkan C
+#include <vulkan/vk_platform.h>
+
+// Vulkan CPP
 #if defined(__INTELLISENSE__) || !defined(USE_CPP20_MODULES)
 #include <vulkan/vulkan_raii.hpp>
 #else
 import vulkan;
 #endif
 
-#include <cstdlib>
-import std;
-
+// SDL3
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_vulkan.h>
+
+// Validation layers
+const std::vector<char const*> VALIDATION_LAYERS = {"VK_LAYER_KHRONOS_validation"};
+
+#ifdef NDEBUG
+constexpr bool ENABLE_VALIDATION_LAYERS = false;
+#else
+constexpr bool ENABLE_VALIDATION_LAYERS = true;
+#endif
 
 namespace ArcticEngine {
 class Application {
   public:
     void Run() {
         VulkanCreateInstance();
+        SetupDebugMessenger();
         CreateWindow();
         VulkanCreateSurface();
         VulkanSelectPhysicalDevice();
@@ -25,25 +45,141 @@ class Application {
     }
 
   private:
-    vk::raii::Instance   instance = nullptr;
-    vk::raii::SurfaceKHR surface  = nullptr;
-    SDL_Window*          window   = nullptr;
+    vk::raii::Context                context;
+    vk::raii::Instance               instance       = nullptr;
+    vk::raii::SurfaceKHR             surface        = nullptr;
+    vk::raii::DebugUtilsMessengerEXT debugMessenger = nullptr;
+    SDL_Window*                      window         = nullptr;
+
+    static VKAPI_ATTR vk::Bool32 VKAPI_CALL debugCallback(vk::DebugUtilsMessageSeverityFlagBitsEXT      severity,
+                                                          vk::DebugUtilsMessageTypeFlagsEXT             type,
+                                                          const vk::DebugUtilsMessengerCallbackDataEXT* pCallbackData,
+                                                          void*                                         pUserData) {
+        std::print(std::cerr, "[Vulkan] Validation layer: type {0} msg: {1}\n", to_string(type),
+                   pCallbackData->pMessage);
+        return vk::False;
+    }
+
+    void SetupDebugMessenger() {
+        if (!ENABLE_VALIDATION_LAYERS)
+            return;
+
+        vk::DebugUtilsMessageSeverityFlagsEXT severityFlags(vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose |
+                                                            vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning |
+                                                            vk::DebugUtilsMessageSeverityFlagBitsEXT::eError);
+
+        vk::DebugUtilsMessageTypeFlagsEXT messageTypeFlags(vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral |
+                                                           vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance |
+                                                           vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation);
+
+        vk::DebugUtilsMessengerCreateInfoEXT debugUtilsMessengerCreateInfoEXT{.messageSeverity = severityFlags,
+                                                                              .messageType     = messageTypeFlags,
+                                                                              .pfnUserCallback = &debugCallback};
+
+        this->debugMessenger = this->instance.createDebugUtilsMessengerEXT(debugUtilsMessengerCreateInfoEXT);
+    }
 
     void VulkanCreateInstance() {
-        std::print("[Vulkan] Initializing...\n");
+        // Get required vulkan validation layers
+        std::print("[Vulkan] Querying Vulkan validation layers...\n");
+
+        std::vector<const char*> requiredLayers;
+        if (ENABLE_VALIDATION_LAYERS) {
+            std::print("[Vulkan] Enabling validation layers...\n");
+            requiredLayers.assign(VALIDATION_LAYERS.begin(), VALIDATION_LAYERS.end());
+        }
+
+        // Check if the required validation layers are supported by the Vulkan implementation
+        std::vector<vk::LayerProperties> layerProperties = this->context.enumerateInstanceLayerProperties();
+
+        if (ENABLE_VALIDATION_LAYERS) {
+            std::print("  Available instance layers:\n");
+            for (const auto& layer : layerProperties) {
+                std::print("    {}\n", std::string(layer.layerName));
+            }
+        }
+
+        for (const char* requiredLayer : requiredLayers) {
+            bool found = std::ranges::any_of(layerProperties, [&](const vk::LayerProperties& layer) {
+                return std::string_view(layer.layerName) == requiredLayer;
+            });
+            if (!found) {
+                std::print("[vulkan] Error: Required layer not supported: {}\n", requiredLayer);
+                Cleanup();
+                std::exit(EXIT_FAILURE);
+            }
+        }
+
+        // Get required SDL Vulkan extensions
+        std::print("[Vulkan] Querying SDL Vulkan extensions...\n");
+        std::vector<const char*> requiredExtensions;
+
+        // Add SDL extensions to the list of required extensions
+        SDL_Init(SDL_INIT_VIDEO);
+        uint32_t           sdlExtensionCount   = 0;
+        char const* const* sdlExtensionsCStyle = SDL_Vulkan_GetInstanceExtensions(&sdlExtensionCount);
+        requiredExtensions.insert(requiredExtensions.end(), sdlExtensionsCStyle,
+                                  sdlExtensionsCStyle + sdlExtensionCount);
+
+        // Add other extensions
+        requiredExtensions.emplace_back("VK_KHR_portability_enumeration"); // mandatory
+        if (ENABLE_VALIDATION_LAYERS) {
+            requiredExtensions.emplace_back(vk::EXTDebugUtilsExtensionName);
+        }
+
+        // Debug extension
+        if (ENABLE_VALIDATION_LAYERS) {
+            std::print("  Required extensions:\n");
+            for (const auto& extension : requiredExtensions) {
+                std::print("    {}\n", std::string_view(extension));
+            }
+        }
+
+        // Check if the required extensions are supported by the Vulkan implementation
+        std::vector<vk::ExtensionProperties> availableExtensions = context.enumerateInstanceExtensionProperties();
+        if (ENABLE_VALIDATION_LAYERS) {
+            std::print("  Available extensions:\n");
+            for (const auto& extension : availableExtensions) {
+                std::print("    {}\n", std::string_view(extension.extensionName));
+            }
+        }
+
+        for (std::string_view requiredExtension : requiredExtensions) {
+            bool isSupported =
+                std::ranges::any_of(availableExtensions, [requiredExtension](const vk::ExtensionProperties& prop) {
+                    return std::string_view(prop.extensionName) == requiredExtension;
+                });
+            if (!isSupported)
+                throw std::runtime_error(std::format("Required extension not supported: {}", requiredExtension));
+        }
 
         // Create instance
-        constexpr vk::ApplicationInfo APP_INFO{
-            .sType              = vk::StructureType::eApplicationInfo,
-            .pApplicationName   = "Hello Vulkan",
-            .applicationVersion = vk::makeVersion(1, 0, 0),
-            .pEngineName        = "No Engine",
-            .engineVersion      = vk::makeVersion(1, 0, 0),
-            .apiVersion         = vk::ApiVersion14,
-        };
-        vk::InstanceCreateInfo createInfo{.pApplicationInfo = &APP_INFO};
-        vk::raii::Context      context;
-        instance = vk::raii::Instance(context, createInfo);
+        std::print("[Vulkan] Creating instance...\n");
+        vk::ApplicationInfo appInfo{.sType              = vk::StructureType::eApplicationInfo,
+                                    .pApplicationName   = "Arctic Game",
+                                    .applicationVersion = vk::makeVersion(1, 0, 0),
+                                    .pEngineName        = "Arctic Engine",
+                                    .engineVersion      = vk::makeVersion(1, 0, 0),
+                                    .apiVersion         = vk::ApiVersion14};
+
+        vk::InstanceCreateInfo createInfo{.pApplicationInfo        = &appInfo,
+                                          .enabledLayerCount       = static_cast<uint32_t>(requiredLayers.size()),
+                                          .ppEnabledLayerNames     = requiredLayers.data(),
+                                          .enabledExtensionCount   = static_cast<uint32_t>(requiredExtensions.size()),
+                                          .ppEnabledExtensionNames = requiredExtensions.data(),
+                                          .pNext};
+
+        if (ENABLE_VALIDATION_LAYERS) {
+            createInfo.setPNext(&debugCreateInfo);
+        }
+
+        try {
+            this->instance = vk::raii::Instance(context, createInfo);
+        } catch (const std::exception& err) {
+            std::print("[vulkan] Error: Instance creation failed: {}\n", err.what());
+            Cleanup();
+            std::exit(EXIT_FAILURE);
+        }
     }
 
     void CreateWindow() {
@@ -57,7 +193,7 @@ class Application {
 
         if (!window) {
             std::print("[Window] Failed to initialize: {0}\n", SDL_GetError());
-            SDL_Quit();
+            Cleanup();
             std::exit(EXIT_FAILURE);
         }
     }
@@ -65,8 +201,8 @@ class Application {
     void VulkanCreateSurface() {
         std::print("[Vulkan] Creating surface...\n");
         VkSurfaceKHR rawSurface = nullptr;
-        SDL_Vulkan_CreateSurface(window, *instance, nullptr, &rawSurface);
-        surface = vk::raii::SurfaceKHR(instance, rawSurface);
+        SDL_Vulkan_CreateSurface(window, *this->instance, nullptr, &rawSurface);
+        this->surface = vk::raii::SurfaceKHR(this->instance, rawSurface);
     }
 
     void VulkanSelectPhysicalDevice() {
